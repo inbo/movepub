56c1eb0a5e091e5535e223fa6693024550d87e35
peterdesmet <peter.desmet@inbo.be>
2022-01-05 14:07:40 +0100

    Use x as label parameter and make it case-insensitive

--- R/get_movebank_term.R
+++ R/get_movebank_term.R
@@ -3,22 +3,27 @@
 #' Get information for a term in the [Movebank Attribute
 #' Dictionary](http://vocab.nerc.ac.uk/collection/MVB/current/).
 #'
-#' @param label Preferred label of the term.
+#' @param x Preferred label of the term to look for. Case will be ignored and `-`, `.` and `:` interpreted as space.
 #' @return List object with term information.
 #' @export
 #' @examples
 #' # Get information for term "animal ID"
 #' get_movebank_term("animal ID")
-get_movebank_term <- function(label) {
+get_movebank_term <- function(x) {
+  label <- gsub("(-|\\.|:)", " ", x)
+
+  # Get concepts
   vocab_url <- file.path(
     "http://vocab.nerc.ac.uk/collection/MVB/current",
     "?_profile=nvs&_mediatype=application/ld+json"
   )
   vocab <- jsonlite::fromJSON(vocab_url, simplifyDataFrame = FALSE)
-
   concepts <- purrr::keep(vocab$`@graph`, ~.$`@type` == "skos:Concept")
-  concept <- purrr::keep(terms, ~.$prefLabel$`@value` == label)
 
+  # Search for concept
+  concept <- purrr::keep(terms, function(x) {
+    tolower(x$prefLabel$`@value`) == label
+  })
   assertthat::assert_that(
     length(concept) > 0,
     msg = glue::glue(
@@ -27,7 +32,8 @@ get_movebank_term <- function(label) {
   )
   concept <- concept[[1]]
 
-  term <- list(
+  # Prepare output
+  list(
     # Identifiers
     id = concept$`@id`,
     # @type: "skos:Concept" for all
@@ -58,6 +64,4 @@ get_movebank_term <- function(label) {
     # note$`@language`: "en" for all
     note = concept$note$`@value`
   )
-
-  return(term)
 }
