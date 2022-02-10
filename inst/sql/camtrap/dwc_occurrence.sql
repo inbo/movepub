f681d6091c9bc5f148ec3783ddc6a1ad214894e9
peterdesmet <peter.desmet@inbo.be>
2022-02-10 19:44:42 +0100

    Remove double quotes

--- inst/sql/camtrap/dwc_occurrence.sql
+++ inst/sql/camtrap/dwc_occurrence.sql
@@ -60,7 +60,7 @@ _id                             N
 SELECT
 -- RECORD-LEVEL
 -- type                         STATIC VALUE
-  'Event' AS "type",
+  'Event' AS type,
 -- language
 -- license
 -- rightsHolder
@@ -70,110 +70,110 @@ SELECT
 -- collectionCode
 -- datasetName
 -- basisOfRecord                STATIC VALUE
-  'MachineObservation' AS "basisOfRecord",
+  'MachineObservation' AS basisOfRecord,
 -- informationWithheld
 -- dataGeneralizations
 -- dynamicProperties
 
 -- OCCURRENCE
 -- occurrenceID
-  obs."observationID" AS "occurrenceID",
+  obs.observationID AS occurrenceID,
 -- individualCount
-  obs."count" AS "individualCount",
+  obs.count AS individualCount,
 -- sex
-  obs."sex" AS "sex",
+  obs.sex AS sex,
 -- lifeStage
-  obs."lifeStage" AS "lifeStage",
+  obs.lifeStage AS lifeStage,
 -- behavior
-  obs."behaviour" AS "behavior",
+  obs.behaviour AS behavior,
 -- occurrenceStatus             STATIC VALUE
-  'present' AS "occurrenceStatus",
+  'present' AS occurrenceStatus,
 -- occurrenceRemarks
-  obs."comments" AS "occurrenceRemarks",
+  obs.comments AS occurrenceRemarks,
 
 -- ORGANISM
 -- organismID
-  obs."individualID" AS "organismID",
+  obs.individualID AS organismID,
 
 -- MATERIALSAMPLE
 -- Not applicable
 
 -- EVENT
 -- eventID
-  obs."sequenceID" AS "eventID",
+  obs.sequenceID AS eventID,
 -- parentEventID
-  obs."deploymentID" AS "parentEventID",
+  obs.deploymentID AS parentEventID,
 -- eventDate                    ISO-8601 in UTC
-  strftime('%Y-%m-%dT%H:%M:%SZ', datetime(obs.timestamp, 'unixepoch')) AS "eventDate",
+  strftime('%Y-%m-%dT%H:%M:%SZ', datetime(obs.timestamp, 'unixepoch')) AS eventDate,
 -- eventTime                    Included in eventDate
 -- habitat
-  dep."habitat" AS habitat,
+  dep.habitat AS habitat,
 -- samplingProtocol
   'camera trap' ||
   CASE
-    WHEN dep."baitUse" IS 'none' THEN ' without bait'
-    WHEN dep."baitUse" IS NOT NULL THEN ' with bait'
-  END AS "samplingProtocol",
+    WHEN dep.baitUse IS 'none' THEN ' without bait'
+    WHEN dep.baitUse IS NOT NULL THEN ' with bait'
+  END AS samplingProtocol,
 -- samplingEffort               Duration of deployment
   strftime('%Y-%m-%dT%H:%M:%SZ', datetime(dep.start, 'unixepoch')) ||
   '/' ||
-  strftime('%Y-%m-%dT%H:%M:%SZ', datetime(dep.end, 'unixepoch')) AS "samplingEffort",
+  strftime('%Y-%m-%dT%H:%M:%SZ', datetime(dep.end, 'unixepoch')) AS samplingEffort,
 -- eventRemarks
   CASE
-    WHEN dep."comments" IS NOT NULL THEN 'comments: ' || dep."comments"
+    WHEN dep.comments IS NOT NULL THEN 'comments: ' || dep.comments
   END ||
   CASE
-    WHEN dep."comments" IS NOT NULL AND dep."tags" IS NOT NULL THEN ' | '
+    WHEN dep.comments IS NOT NULL AND dep.tags IS NOT NULL THEN ' | '
   END ||
   CASE
-    WHEN dep."tags" IS NOT NULL THEN 'tags: ' || dep."tags"
-  END AS "eventRemarks",
+    WHEN dep.tags IS NOT NULL THEN 'tags: ' || dep.tags
+  END AS eventRemarks,
 
 -- LOCATION
 -- locationID
-  dep."locationID" AS "locationID",
+  dep.locationID AS locationID,
 -- continent
 -- countryCode
 -- locality
-  dep."locationName" AS "locality",
+  dep.locationName AS locality,
 -- locationRemarks
-  dep."featureType" AS "locationRemarks",
+  dep.featureType AS locationRemarks,
 -- decimalLatitude
-  dep."latitude" AS "decimalLatitude",
+  dep.latitude AS decimalLatitude,
 -- decimalLongitude
-  dep."longitude" AS "decimalLongitude",
--- geodeticDatum              STATIC VALUE
-  'WGS84' AS "geodeticDatum",
+  dep.longitude AS decimalLongitude,
+-- geodeticDatum                STATIC VALUE
+  'WGS84' AS geodeticDatum,
 -- coordinateUncertaintyInMeters
-  dep."coordinateUncertainty" AS "coordinateUncertaintyInMeters",
+  dep.coordinateUncertainty AS coordinateUncertaintyInMeters,
 
 -- GEOLOGICAL CONTEXT
 -- Not applicable
 
 -- IDENTIFICATION
 -- identifiedBy
-  obs."classifiedBy" AS "identifiedBy",
+  obs.classifiedBy AS identifiedBy,
 -- identifiedByID
 -- dateIdentified               ISO-8601 in UTC
-  strftime('%Y-%m-%dT%H:%M:%SZ', datetime(obs.classificationTimestamp, 'unixepoch')) AS "dateIdentified",
+  strftime('%Y-%m-%dT%H:%M:%SZ', datetime(obs.classificationTimestamp, 'unixepoch')) AS dateIdentified,
 -- identificationRemarks
   CASE
-    WHEN obs."classificationMethod" IS NOT NULL THEN 'classificationMethod: ' || obs."classificationMethod"
+    WHEN obs.classificationMethod IS NOT NULL THEN 'classificationMethod: ' || obs.classificationMethod
   END ||
   CASE
-    WHEN obs."classificationMethod" IS NOT NULL AND obs."classificationConfidence" IS NOT NULL THEN ' | '
+    WHEN obs.classificationMethod IS NOT NULL AND obs.classificationConfidence IS NOT NULL THEN ' | '
   END ||
   CASE
-    WHEN obs."classificationConfidence" IS NOT NULL THEN 'classificationConfidence: ' || obs."classificationConfidence"
-  END AS "identificationRemarks",
+    WHEN obs.classificationConfidence IS NOT NULL THEN 'classificationConfidence: ' || obs.classificationConfidence
+  END AS identificationRemarks,
 
 -- TAXON
 -- taxonID
-  obs."taxonID" AS "taxonID",
+  obs.taxonID AS taxonID,
 -- scientificName
-  obs."scientificName" AS "scientificName",
+  obs.scientificName AS scientificName,
 -- kingdom                      STATIC VALUE: in almost all use cases, it is safe to assume all observations are animals
-  'Animalia' AS "kingdom"
+  'Animalia' AS kingdom
 -- taxonRank
 -- vernacularName
 
@@ -181,12 +181,12 @@ FROM
   observations AS obs
 
   LEFT JOIN deployments AS dep
-    ON obs."deploymentID" = dep."deploymentID"
+    ON obs.deploymentID = dep.deploymentID
 
 WHERE
   -- Select biological observations only (excluding observations marked as human, blank, vehicle)
   -- Same filter should be used in dwc_multimedia.sql
-  obs."observationType" = 'animal'
+  obs.observationType = 'animal'
 
 ORDER BY
-  obs."observationID"
+  obs.observationID
