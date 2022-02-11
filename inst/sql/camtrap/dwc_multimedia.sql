f25d6c4d8b19c8e7d551f60b55f2f70f0d26f4f9
peterdesmet <peter.desmet@inbo.be>
2022-02-11 12:46:07 +0100

    Update multimedia SQL to Audubon Core

--- inst/sql/camtrap/dwc_multimedia.sql
+++ inst/sql/camtrap/dwc_multimedia.sql
@@ -1,6 +1,7 @@
 /*
 Created by Peter Desmet (INBO)
 Mapping from Camtrap DP: https://tdwg.github.io/camtrap-dp
+Mapping to Audubon Media Description: https://rs.gbif.org/extension/ac/audubon_2020_10_06.xml
 Y = included in DwC, N = not included in DwC
 
 CAMTRAP DP MEDIA
@@ -21,65 +22,57 @@ _id                             N
 */
 
 -- Observations can be based on sequences (sequenceID) or individual files (mediaID)
--- Make two joins and union to capture both cases
+-- Make two joins and union to capture both cases without overlap
 WITH observations_media AS (
 -- Sequence based observations
-  SELECT
-    obs.observationID,
-    med.*
-  FROM
-    observations AS obs
-    LEFT JOIN
-      media AS med
-      ON obs.sequenceID = med.sequenceID
-  WHERE
-    obs.mediaID IS NULL
-    AND obs.observationType = 'animal'
+  SELECT obs.observationID, obs.timestamp AS observationTimestamp, med.*
+  FROM observations AS obs
+    LEFT JOIN media AS med ON obs.sequenceID = med.sequenceID
+  WHERE obs.observationType = 'animal' AND obs.mediaID IS NULL
   UNION
--- Media based observations
-  SELECT
-    obs.observationID,
-    med.*
-  FROM
-    observations AS obs
-    LEFT JOIN
-      media AS med
-      ON obs.mediaID = med.mediaID
-  WHERE
-    obs.mediaID IS NOT NULL
-    AND obs.observationType = 'animal'
+-- File based observations
+  SELECT obs.observationID, obs.timestamp AS observationTimestamp, med.*
+  FROM observations AS obs
+    LEFT JOIN media AS med ON obs.mediaID = med.mediaID
+  WHERE obs.observationType = 'animal' AND obs.mediaID IS NOT NULL
 )
 
 SELECT
 -- occurrenceID
   obs_med.observationID AS occurrenceID,
+-- creator
+-- providerLiteral
+-- provider
+-- rights
+  {metadata$mediaLicense} AS rights,
+-- owner
+-- identifier
+  obs_med.mediaID AS identifier,
 -- type
   CASE
     WHEN obs_med.fileMediatype LIKE '%video%' THEN 'MovingImage'
     ELSE 'StillImage'
   END AS type,
+-- providerManagedID
+  obs_med._id AS providerManagedID,
+-- captureDevice
+--  dep.cameraModel AS captureDevice,
+-- resourceCreationTechnique
+  obs_med.captureMethod AS resourceCreationTechnique,
+-- accessURI
+  obs_med.filePath AS accessURI,
 -- format
   obs_med.fileMediatype AS format,
--- identifier
-  obs_med.filePath AS identifier,
--- references
--- title
--- description
--- created
-  STRFTIME('%Y-%m-%dT%H:%M:%SZ', datetime(obs_med.timestamp, 'unixepoch')) AS created
--- creator
--- contributor
--- publisher
--- audience
--- source
--- license                              Only available in dataset metadata
--- rightsHolder                         Only available in dataset metadata
--- datasetID                            Only available in dataset metadata
+-- CreateDate
+  STRFTIME('%Y-%m-%dT%H:%M:%SZ', datetime(obs_med.timestamp, 'unixepoch')) AS createDate
 
 FROM
   observations_media AS obs_med
+  LEFT JOIN deployments AS dep
+    ON obs_med.deploymentID = dep.deploymentID
 
 ORDER BY
 -- Order is not retained in observations_media, so important to sort
-  obs_med."timestamp",
-  obs_med."fileName"
+  obs_med.observationTimestamp,
+  obs_med.timestamp,
+  obs_med.fileName
