1e8cd20a859688a5c62514622674aa0da24aaeeb
peterdesmet <peter.desmet@inbo.be>
2022-04-15 18:31:20 +0200

    Add GPS SQL

--- inst/sql/movebank_dwc_occurrence.sql
+++ inst/sql/movebank_dwc_occurrence.sql
@@ -1,8 +1,10 @@
 /*
 Created by Peter Desmet (INBO)
 */
+
+/* RECORD-LEVEL */
+
 SELECT
--- RECORD-LEVEL
   'Event'                               AS type,
   'TODO'                                AS license,
   'TODO'                                AS rightsHolder,
@@ -10,9 +12,16 @@ SELECT
   'TODO'                                AS institionCode,
   'TODO'                                AS collectionCode,
   'TODO'                                AS datasetName,
+  *
+FROM (
+
+/* DEPLOYMENTS */
+
+SELECT
+-- RECORD-LEVEL
   'HumanObservation'                    AS basisOfRecord,
   'TODO'                                AS informationWithheld,
-
+  NULL                                  AS dataGeneralizations,
 -- OCCURRENCE
   ref."deployment-id"                   AS occurrenceID,
   CASE
@@ -31,13 +40,66 @@ SELECT
   STRFTIME('%Y-%m-%dT%H:%M:%SZ', ref."deploy-on-date") AS eventDate,
   ref."deployment-comments"             AS eventRemarks,
 -- LOCATION
+  NULL                                  AS minimumDistanceAboveSurfaceInMeters,
   ref."deploy-on-latitude"              AS decimalLatitude,
   ref."deploy-on-longitude"             AS decimalLongitude,
   'WGS84'                               AS geodeticDatum,
-  'TODO'                                AS coordinateUncertaintyInMeters,
+  30                                    AS coordinateUncertaintyInMeters,
 -- TAXON
   ref."animal-taxon"                    AS scientificName,
   'Animalia'                            AS kingdom
-
 FROM
   reference_data AS ref
+
+UNION
+
+/* GPS POSITIONS */
+
+SELECT
+-- RECORD-LEVEL
+  'MachineObservation'                  AS basisOfRecord,
+  'TODO'                                AS informationWithheld,
+  'subsampled by hour: first of ' || gps."subsample-count" || ' records' AS dataGeneralizations,
+-- OCCURRENCE
+  gps."event-id" || ''                  AS occurrenceID,
+  NULL                                  AS sex,
+  NULL                                  AS lifeStage,
+  'present'                             AS occurrenceStatus,
+-- ORGANISM
+  ref."animal-id"                       AS organismID,
+  ref."animal-nickname"                 AS organismName,
+-- EVENT
+  ref."tag-id" ||  '-' || ref."animal-id" AS eventID,
+  gps."sensor-type"                     AS samplingProtocol,
+  STRFTIME('%Y-%m-%dT%H:%M:%SZ', gps."timestamp") AS eventDate,
+  NULL                                  AS eventRemarks,
+-- LOCATION
+  gps."height-above-msl"                AS minimumDistanceAboveSurfaceInMeters,
+  gps."location-lat"                    AS decimalLatitude,
+  gps."location-lat"                    AS decimalLongitude,
+  'WGS84'                               AS geodeticDatum,
+  gps."location-error-numerical"        AS coordinateUncertaintyInMeters,
+-- TAXON
+  ref."animal-taxon"                    AS scientificName,
+  'Animalia'                            AS kingdom
+FROM
+  (
+    SELECT
+      *,
+      COUNT(*) AS "subsample-count"
+    FROM gps
+    WHERE
+    -- Exclude outliers
+      visible = 1
+    GROUP BY
+    -- Group by date+hour
+      STRFTIME('%Y-%m-%dT%H', "timestamp")
+    HAVING
+    -- Take first record within group, i.e. first timestamp
+    -- Movebank data are sorted by timestamp: https://github.com/tdwg/dwc-for-biologging/issues/31
+      ROWID = MIN(ROWID)
+  ) AS gps
+  LEFT JOIN reference_data AS ref
+    ON gps."tag-local-identifier" = ref."tag-id"
+    AND gps."individual-local-identifier" = ref."animal-id"
+)
