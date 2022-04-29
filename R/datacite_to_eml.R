f24c5e594903114c971d64eeb189756b007a7e2a
peterdesmet <peter.desmet@inbo.be>
2022-04-29 15:53:00 +0200

    Don't set organizationName

--- R/datacite_to_eml.R
+++ R/datacite_to_eml.R
@@ -24,8 +24,8 @@ datacite_to_eml <- function(doi) {
   creators <- purrr::map(metadata$creators, ~ EML::set_responsibleParty(
     givenName = .$givenName,
     surName = .$familyName,
-    organizationName = .$affiliation,
     userId = .$nameIdentifiers[[1]]$nameIdentifier
+    # organizationName: not set to .$affilation, because intended for non-indiv.
   ))
 
   # Create eml
