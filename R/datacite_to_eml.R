33bd748d00898a407dec6970dcd5a7ac9a3dfc0d
peterdesmet <peter.desmet@inbo.be>
2022-05-02 14:40:42 +0200

    Add contact, metadata provider

--- R/datacite_to_eml.R
+++ R/datacite_to_eml.R
@@ -4,10 +4,15 @@
 #' uploaded to a [GBIF IPT](https://www.gbif.org/ipt).
 #'
 #' @param doi DOI of a dataset.
+#' @param contact One or more persons to be set as resource contact. Provide as
+#'   person objects, e.g. `person("Peter", "Desmet", , "fakeaddress@email.com",
+#'   "mdc", c(ORCID = "0000-0002-8442-8025"))`.
+#' @param metadata_provider One or more person to be set as metadata provider.
+#'   Same format as `contact`.
 #' @return EML list that can be extended and/or written to file with
 #' [EML::write_eml()].
 #' @export
-datacite_to_eml <- function(doi) {
+datacite_to_eml <- function(doi, contact, metadata_provider = contact) {
   # Read metadata from DataCite
   doi <- gsub("https://doi.org/", "", doi)
   result <- jsonlite::read_json(paste0("https://api.datacite.org/dois/", doi))
@@ -35,16 +40,16 @@ datacite_to_eml <- function(doi) {
       abstract = list(
         para = purrr::map_chr(metadata$descriptions, function(x) {
           description <- x$description
-          if (grepl("</", description)) { # Description contains HTML
-            paste0("<![CDATA[", description, "]]>")
+          if (grepl("</", description)) {
+            paste0("<![CDATA[", description, "]]>") # Wrap HTML
           } else {
             description
           }
         })
       ),
-      contact = NULL, # TODO
+      contact = contacts,
       creator = creators,
-      metadataProvider = NULL, # TODO
+      metadataProvider = metadata_providers,
       keywordsSet = list(
         list(
           keywordThesaurus = "n/a",
