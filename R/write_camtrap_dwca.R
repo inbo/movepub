f6f13516db2b710b1ea391c593fa80ef5be38e6c
peterdesmet <peter.desmet@inbo.be>
2022-05-04 17:12:12 +0200

    Hide camtrap function

--- R/write_camtrap_dwca.R
+++ R/write_camtrap_dwca.R
@@ -12,7 +12,6 @@
 #'   [frictionless::read_package()].
 #' @param directory Path to local directory to write files to.
 #' @return Darwin Core formatted CSV files written to disk.
-#' @export
 write_camtrap_dwca <- function(package, directory) {
   # Read data from Data Package
   deployments <- frictionless::read_resource(package, "deployments")
