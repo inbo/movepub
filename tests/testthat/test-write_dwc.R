0e79c5bc5c13c6a921966335ef095875e56628ad
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-28 16:31:07 +0200

    test writing to path

--- tests/testthat/test-write_dwc.R
+++ tests/testthat/test-write_dwc.R
@@ -1,2 +1,9 @@
 dir.create(file.path(tempdir()), "movepub")
 temp_dir <- file.path(tempdir(), "movepub")
+test_that("write_dwc() writes csv file to a path", {
+  expect_true(file.exists(file.path(temp_dir, "dwc_occurrence.csv")))
+})
+
+test_that("write_dwc() writes EML file to a path", {
+  expect_true(file.exists(file.path(temp_dir, "eml.xml")))
+})
