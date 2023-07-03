7d31936f5bda71b3b4cf2a6947294db87f4565dd
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-07-03 15:28:52 +0200

    Add helper for removing UUIDs from strings

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -19,3 +19,32 @@ remove_temp_path <- function(string, replacement = "temporary_path") {
   gsub("\\/tmp\\/[a-zA-Z]+\\/movepub",replacement,string)
 }
 
+#' Remove a UUID from a character
+#'
+#' Helper for test-write_dwc.R.
+#' This helper is convenient for file snapshots where a UUID is included in the file, yet different every run.
+#' This is a common artefact of how frictionless data packages are created using frictionless-r.
+#' If this behaviour in frictionless-r is changed in the future, this helper becomes unneccesairy.
+#'
+#' @param string Character vector. Of which UUIDs need to be removed.
+#' @param replacement Character (Optional). A replacement for the matched UUID.
+#' By default \code{"RANDOM_UUID"}
+#'
+#' @return A Character vector with the UUIDs removed.
+#' @family helper functions
+#'
+#' @examples
+#' to_clean <- paste(
+#'     'encoding=\"UTF-8\"?>",',
+#'     '"<eml:eml xmlns:eml=\"https://eml.ecoinformatics.org/eml-2.2.0\"',
+#'     'packageId=\"39272b1c-4174-4a86-a2d2-f48c4f29e6de\"',
+#'     'system=\"uuid\"',
+#'     collapse = " ")
+#' remove_UUID(to_clean)
+remove_UUID <- function(string, replacement = "RANDOM_UUID") {
+  gsub("[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}",
+       replacement,
+       string)
+}
+
+
