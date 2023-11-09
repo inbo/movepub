8a4c99abac444b722e2c485a35da4165ef9beb2c
Peter Desmet <peter.desmet@inbo.be>
2023-11-09 10:58:52 +0100

    Update O_ASSEN to point to latest version

--- R/data.R
+++ R/data.R
@@ -6,15 +6,15 @@
 #' [frictionless::read_package()].
 #'
 #' This sample is derived from the Zenodo-deposited dataset
-#' [Dijkstra et al. (2022)](https://doi.org/10.5281/zenodo.5653311), but
+#' [Dijkstra et al. (2022)](https://doi.org/10.5281/zenodo.10053903), but
 #' excludes the acceleration data.
-#' @source https://zenodo.org/record/5653311
+#' @source https://doi.org/10.5281/zenodo.10053903
 #' @family sample data
 #' @examples
 #' \dontrun{
 #' # The data in o_assen was created with the code below
 #' o_assen <-
-#'   frictionless::read_package("https://zenodo.org/record/5653311/files/datapackage.json") %>%
+#'   frictionless::read_package("https://zenodo.org/records/10053903/files/datapackage.json") %>%
 #'   frictionless::remove_resource("acceleration")
 #' usethis::use_data(o_assen)
 #' }
