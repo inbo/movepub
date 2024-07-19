d595d242083dacc957377d3f8118c41b70b2738c
Peter Desmet <peter.desmet@inbo.be>
2024-07-19 15:02:22 +0200

    Reorganize write_dwc() tests

--- tests/testthat/test-write_dwc.R
+++ tests/testthat/test-write_dwc.R
@@ -1,43 +1,41 @@
-# Create temporary files
-temp_dir <- file.path(tempdir(), "dwc")
-dir.create(temp_dir)
-
 test_that("write_dwc() returns error on missing or malformed doi", {
-  package_no_doi <- o_assen
-  package_no_doi$id <- NULL
+  x_no_doi <- o_assen
+  x_no_doi$id <- NULL
+  temp_dir <- file.path(tempdir(), "dwc")
+  on.exit(unlink(temp_dir, recursive = TRUE))
+
   expect_error(
-    write_dwc(package_no_doi, temp_dir),
+    write_dwc(x_no_doi, temp_dir),
     class = "movepub_error_doi_missing"
   )
   expect_error(
-    write_dwc(package_no_doi, temp_dir, doi = c("a", "b", "c")),
+    write_dwc(x_no_doi, temp_dir, doi = c("a", "b", "c")),
     class = "movepub_error_doi_invalid"
   )
   expect_error(
-    write_dwc(package_no_doi, temp_dir, doi = 10.5281),
+    write_dwc(x_no_doi, temp_dir, doi = 10.5281),
     class = "movepub_error_doi_invalid"
   )
 })
 
 test_that("write_dwc() returns error on missing resources", {
   skip_if_offline()
-  # Create data package with no reference-data resource
-  package_no_ref_data <-
+  x_no_ref_data <-
     frictionless::remove_resource(o_assen, "reference-data")
+  x_no_gps <-
+    frictionless::remove_resource(o_assen, "gps")
+  temp_dir <- file.path(tempdir(), "dwc")
+  on.exit(unlink(temp_dir, recursive = TRUE))
 
   expect_error(
     suppressMessages(
-      write_dwc(package_no_ref_data, temp_dir, doi = "10.5281/zenodo.5653311")
+      write_dwc(x_no_ref_data, temp_dir, doi = "10.5281/zenodo.5653311")
     ),
     class = "movepub_error_reference_data_missing"
   )
-
-  # Create package with no GPS resource
-  package_no_gps <-
-    frictionless::remove_resource(o_assen, "gps")
   expect_error(
     suppressMessages(
-      write_dwc(package_no_gps, temp_dir, doi = "10.5281/zenodo.5653311")
+      write_dwc(x_no_gps, temp_dir, doi = "10.5281/zenodo.5653311")
     ),
     class = "movepub_error_gps_data_missing"
   )
@@ -46,7 +44,10 @@ test_that("write_dwc() returns error on missing resources", {
 test_that("write_dwc() writes CSV and meta.xml files to a directory and
            a list of data frames invisibly", {
   skip_if_offline()
-  result <- suppressMessages(write_dwc(o_assen, temp_dir))
+  x <- o_assen
+  temp_dir <- file.path(tempdir(), "dwc")
+  on.exit(unlink(temp_dir, recursive = TRUE))
+  result <- suppressMessages(write_dwc(x, temp_dir))
 
   expect_identical(
     list.files(temp_dir),
@@ -58,7 +59,10 @@ test_that("write_dwc() writes CSV and meta.xml files to a directory and
 
 test_that("write_dwc() returns the expected Darwin Core terms as columns", {
   skip_if_offline()
-  result <- suppressMessages(write_dwc(o_assen, temp_dir))
+  x <- o_assen
+  temp_dir <- file.path(tempdir(), "dwc")
+  on.exit(unlink(temp_dir, recursive = TRUE))
+  result <- suppressMessages(write_dwc(x, temp_dir))
 
   expect_identical(
     colnames(result$occurrence),
@@ -102,7 +106,10 @@ test_that("write_dwc() returns the expected Darwin Core terms as columns", {
 test_that("write_dwc() returns the expected Darwin Core mapping for the example
            dataset", {
   skip_if_offline()
-  suppressMessages(write_dwc(o_assen, temp_dir))
+  x <- o_assen
+  temp_dir <- file.path(tempdir(), "dwc")
+  on.exit(unlink(temp_dir, recursive = TRUE))
+  suppressMessages(write_dwc(x, temp_dir))
 
   expect_snapshot_file(file.path(temp_dir, "occurrence.csv"))
   expect_snapshot_file(file.path(temp_dir, "meta.xml"))
@@ -110,27 +117,11 @@ test_that("write_dwc() returns the expected Darwin Core mapping for the example
 
 test_that("write_dwc() returns files that comply with the info in meta.xml", {
   skip_if_offline()
-  suppressMessages(write_dwc(o_assen, temp_dir))
+  x <- o_assen
+  temp_dir <- file.path(tempdir(), "dwc")
+  on.exit(unlink(temp_dir, recursive = TRUE))
+  suppressMessages(write_dwc(x, temp_dir))
 
+  # Use helper function to compare
   expect_meta_match(file.path(temp_dir, "occurrence.csv"))
 })
-
-test_that("write_dwc() returns expected messages" , {
-  skip_if_offline()
-  # One of the headings
-  expect_message(
-    write_dwc(o_assen, temp_dir),
-    "Transforming data to Darwin Core",
-    fixed = TRUE
-  )
-
-  # AphiaID matching
-  expect_message(
-    write_dwc(o_assen, temp_dir),
-    "Haematopus ostralegus: 147436",
-    fixed = TRUE
-  )
-})
-
-# Remove temporary files
-unlink(temp_dir, recursive = TRUE)
