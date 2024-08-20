fdccbfbe26e9748206925eb9a3ceafbd74a704fd
Peter Desmet <peter.desmet@inbo.be>
2024-08-20 15:46:14 +0200

    Merge #83

--- tests/testthat/test-get_aphia_id.R
+++ tests/testthat/test-get_aphia_id.R
@@ -8,7 +8,8 @@ test_that("get_aphia_id() returns the correct output", {
     name = "Mola mola",
     aphia_id = 127405,
     aphia_lsid = "urn:lsid:marinespecies.org:taxname:127405",
-    aphia_url = "https://www.marinespecies.org/aphia.php?p=taxdetails&id=127405"
+    aphia_url = "https://www.marinespecies.org/aphia.php?p=taxdetails&id=127405",
+    aphia_url_cli = "{.href [127405](https://www.marinespecies.org/aphia.php?p=taxdetails&id=127405)}"
   )
   expect_equal(get_aphia_id("Mola mola"), df_mola)
 
@@ -17,7 +18,8 @@ test_that("get_aphia_id() returns the correct output", {
     name = "not_a_name",
     aphia_id = NA,
     aphia_lsid = NA_character_,
-    aphia_url = NA_character_
+    aphia_url = NA_character_,
+    aphia_url_cli = NA_character_
   )
   expect_equal(get_aphia_id("not_a_name"), df_na)
 
@@ -30,7 +32,8 @@ test_that("get_aphia_id() returns the correct output", {
     name = "Pisces",
     aphia_id = 11676,
     aphia_lsid = "urn:lsid:marinespecies.org:taxname:11676",
-    aphia_url = "https://www.marinespecies.org/aphia.php?p=taxdetails&id=11676"
+    aphia_url = "https://www.marinespecies.org/aphia.php?p=taxdetails&id=11676",
+    aphia_url_cli = "{.href [11676](https://www.marinespecies.org/aphia.php?p=taxdetails&id=11676)}"
   )
   expect_equal(get_aphia_id("Pisces"), df_pisces)
 
