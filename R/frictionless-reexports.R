ba36ae8a56f7517a761270466e20a32f0b9a4c40
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2024-08-20 15:40:47 +0200

    drop family

--- R/frictionless-reexports.R
+++ R/frictionless-reexports.R
@@ -1,29 +1,23 @@
 #' @importFrom frictionless create_package
 #' @export
-#' @family frictionless functions
 frictionless::create_package
 
 #' @importFrom frictionless read_package
 #' @export
-#' @family frictionless functions
 frictionless::read_package
 
 #' @importFrom frictionless write_package
 #' @export
-#' @family frictionless functions
 frictionless::write_package
 
 #' @importFrom frictionless resources
 #' @export
-#' @family frictionless functions
 frictionless::resources
 
 #' @importFrom frictionless read_resource
 #' @export
-#' @family frictionless functions
 frictionless::read_resource
 
 #' @importFrom frictionless remove_resource
 #' @export
-#' @family frictionless functions
 frictionless::remove_resource
