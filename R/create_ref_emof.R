dc6d41b06628452b65f10588a1f55d27bc3535e0
Peter Desmet <peter.desmet@inbo.be>
2024-08-20 17:14:11 +0200

    Update create_ref_emof
    - Document function
    - First sex, then lifeStage
    - Map measurementValue as is
    - Use recode, not case_when

--- R/create_ref_emof.R
+++ R/create_ref_emof.R
@@ -1,42 +1,16 @@
-#' Create extended measurements or facts (eMoF)
+#' Create Extended Measurements Or Facts from Darwin Core Occurrence data
+#'
+#' Pulls the **sex** and **life stage** information from the Darwin Core
+#' Occurrence data created with `create_ref_occurrence()` and maps these values
+#' to a controlled vocabulary recommended by [OBIS](https://obis.org/).
 #'
 #' @param ref_occurrence Data frame with Darwin Core occurrences derived from
-#' tag attachments, as returned by `create_ref_occurrence()`.
-#' @return Data frame with
-#' [eMoF](https://rs.gbif.org/extension/obis/extended_measurement_or_fact_2023-08-28.xml),
-#' more specifically `life stage` and `sex`.
+#'   tag attachments, as returned by `create_ref_occurrence()`.
+#' @return Data frame with [Extended Measurement Or Facts](
+#'   https://rs.gbif.org/extension/obis/extended_measurement_or_fact_2023-08-28.xml).
 #' @family dwc functions
 #' @noRd
 create_ref_emof <- function(ref_occurrence) {
-  lifestage <-
-    ref_occurrence %>%
-    dplyr::mutate(
-      .keep = "none",
-      occurrenceID = .data$occurrenceID,
-      measurementType = "life stage",
-      measurementTypeID =
-        "http://vocab.nerc.ac.uk/collection/P01/current/LSTAGE01/",
-      measurementValue = dplyr::case_when(
-        .data$lifeStage == "adult" ~ "adult",
-        .data$lifeStage == "juvenile" ~ "juvenile",
-        .data$lifeStage == "unknown" ~ "indeterminate",
-        is.na(.data$lifeStage) ~ "not speficifed",
-        .default = "indeterminate"
-      ),
-      measurementValueID = dplyr::case_when(
-        measurementValue == "adult" ~
-          "http://vocab.nerc.ac.uk/collection/S11/current/S1116/",
-        measurementValue == "juvenile" ~
-          "https://vocab.nerc.ac.uk/collection/S11/current/S1127/",
-        measurementValue == "indeterminate" ~
-          "http://vocab.nerc.ac.uk/collection/S11/current/S1152/",
-        measurementValue == "not speficied" ~
-          "http://vocab.nerc.ac.uk/collection/S11/current/S1131/"
-      ),
-      measurementUnit = NA,
-      measurementUnitID = "http://vocab.nerc.ac.uk/collection/P06/current/XXXX/"
-    )
-
   sex <-
     ref_occurrence %>%
     dplyr::mutate(
@@ -45,41 +19,51 @@ create_ref_emof <- function(ref_occurrence) {
       measurementType = "sex",
       measurementTypeID =
         "http://vocab.nerc.ac.uk/collection/P01/current/ENTSEX01/",
-      measurementValue = dplyr::case_when(
-        .data$sex == "female" ~ "female",
-        .data$sex == "male" ~ "male",
-        .data$sex == "unknown" ~ "indeterminate",
-        is.na(.data$sex) ~ "not spefified",
-        .default = "indeterminate"
+      measurementValue = .data$sex, # Value as is
+      measurementValueID = dplyr::recode(
+        .data$sex,
+        "female" = "http://vocab.nerc.ac.uk/collection/S10/current/S102/",
+        "male" = "https://vocab.nerc.ac.uk/collection/S10/current/S103/",
+        "unknown" = "https://vocab.nerc.ac.uk/collection/S10/current/S105/", # indeterminate
+        .default = NA_character_, # Don't map other values
+        .missing = "https://vocab.nerc.ac.uk/collection/S10/current/S104/" # not specified
       ),
-      measurementValueID = dplyr::case_when(
-        measurementValue == "female" ~
-          "http://vocab.nerc.ac.uk/collection/S10/current/S102/",
-        measurementValue == "male" ~
-          "https://vocab.nerc.ac.uk/collection/S10/current/S103/",
-        measurementValue == "hermaphrodite" ~
-          "https://vocab.nerc.ac.uk/collection/S10/current/S105/",
-        measurementValue == "indeterminate" ~
-          "https://vocab.nerc.ac.uk/collection/S10/current/S105/",
-        measurementValue == "not specified" ~
-          "https://vocab.nerc.ac.uk/collection/S10/current/S104/"
-      ),
-      measurementUnit = NA,
+      measurementUnit = NA_character_,
       measurementUnitID = "http://vocab.nerc.ac.uk/collection/P06/current/XXXX/"
     )
 
-  emof <- dplyr::bind_rows(lifestage, sex) %>%
+  lifestage <-
+    ref_occurrence %>%
+    dplyr::mutate(
+      .keep = "none",
+      occurrenceID = .data$occurrenceID,
+      measurementType = "life stage",
+      measurementTypeID =
+        "http://vocab.nerc.ac.uk/collection/P01/current/LSTAGE01/",
+      measurementValue = .data$lifeStage, # Value as is
+      measurementValueID = dplyr::recode(
+        .data$lifeStage,
+        "adult" = "http://vocab.nerc.ac.uk/collection/S11/current/S1116/",
+        "subadult" = "https://vocab.nerc.ac.uk/collection/S11/current/S120/", # sub-adult
+        "juvenile" = "https://vocab.nerc.ac.uk/collection/S11/current/S1127/",
+        "unknown" = "http://vocab.nerc.ac.uk/collection/S11/current/S1152/", # indeterminate
+        .default = NA_character_, # Don't map other values
+        .missing = "http://vocab.nerc.ac.uk/collection/S11/current/S1131/" # not specified
+      ),
+      measurementUnit = NA_character_,
+      measurementUnitID = "http://vocab.nerc.ac.uk/collection/P06/current/XXXX/"
+    )
+
+  emof <-
+    dplyr::bind_rows(sex, lifestage) %>%
     dplyr::arrange(.data$occurrenceID)
 
-  # only keep measurementTypes that have at least 1 non-NA value in
-  # `ref_occurrence`
+  # Remove the measurementType if all values of that type are NA in ref_occurrence
   if (all(is.na(ref_occurrence$sex))) {
-    emof <- emof %>%
-      dplyr::filter(.data$measurementType != "sex")
+    emof <- dplyr::filter(emof, .data$measurementType != "sex")
   }
   if (all(is.na(ref_occurrence$lifeStage))) {
-    emof <- emof %>%
-      dplyr::filter(.data$measurementType != "life stage")
+    emof <- dplyr::filter(emof, .data$measurementType != "life stage")
   }
 
   return(emof)
