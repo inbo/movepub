a903a3df0030a3d04c2bb3274e0aa579c42026ad
Peter Desmet <peter.desmet@inbo.be>
2025-08-12 18:45:54 +0200

    Reorder cf documenation, rename helper

--- R/html_to_docbook.R
+++ R/html_to_docbook.R
@@ -18,6 +18,7 @@
 #' --- | ---
 #' `<p>...</p>` | `<para>...</para>`
 #' `<div>...</div>` | `<para>...</para>`
+#' `<h1>...</h1>` | `<title>...</title>`
 #' `<h2>...</h2>` | `<para>...</para>`
 #' `<h3>...</h3>` | `<para>...</para>`
 #' `<h4>...</h4>` | `<para>...</para>`
@@ -26,15 +27,14 @@
 #' `<ul>...</ul>` | `<itemizedlist>...</itemizedlist>`
 #' `<ol>...</ol>` | `<orderedlist>...</orderedlist>`
 #' `<li>...</li>` | `<listitem>...</listitem>`
-#' `<strong>...</strong>` | `<emphasis>...</emphasis>`
-#' `<b>...</b>` | `<emphasis>...</emphasis>`
 #' `<em>...</em>` | `<emphasis>...</emphasis>`
 #' `<i>...</i>` | `<emphasis>...</emphasis>`
+#' `<strong>...</strong>` | `<emphasis>...</emphasis>`
+#' `<b>...</b>` | `<emphasis>...</emphasis>`
 #' `<sub>...</sub>` | `<subscript>...</subscript>`
 #' `<sup>...</sup>` | `<superscript>...</superscript>`
 #' `<pre>...</pre>` | `<literalLayout>...</literalLayout>`
 #' `<a href="http://example.com">...</a>` | `<ulink url="https://example.com"><citetitle>...</citetitle></ulink>`
-#' `<h1>...</h1>` | `<title>...</title>`
 #'
 #' @examples
 #' html_to_docbook("<div>My <b>bold</b> text.</div>")
@@ -42,35 +42,40 @@ html_to_docbook <- function(string) {
   # Necessary for empty values and non-HTML text
   doc <- xml2::read_html(paste0("<root>", string, "</root>"))
   root <- xml2::xml_find_first(doc, ".//body/root")
-  paste(purrr::map_chr(xml2::xml_contents(root), convert), collapse = "")
+  paste(
+    purrr::map_chr(xml2::xml_contents(root), convert_xml_node),
+    collapse = ""
+  )
 }
 
-#' Helper function to recursively convert XML nodes to DocBook XML
+#' Convert XML node to DocBook.
+#'
+#' Helper function to recursively convert XML nodes to DocBook.
 #'
 #' @param node An XML node to convert.
-#' @returns A string with HTML converted to Dokbook XML
+#' @returns A string with HTML converted to DocBook.
 #' @noRd
-convert <- function(node) {
+convert_xml_node <- function(node) {
   tag_map <- list(
-    div = "para",
     p = "para",
-    b = "emphasis",
-    strong = "emphasis",
-    i = "emphasis",
-    em = "emphasis",
-    a = "ulink",
-    ul = "itemizedlist",
-    ol = "orderedlist",
-    li = "listitem",
-    sup = "superscript",
-    sub = "subscript",
-    pre = "literalLayout",
+    div = "para",
     h1 = "title",
     h2 = "para",
     h3 = "para",
     h4 = "para",
     h5 = "para",
-    h6 = "para"
+    h6 = "para",
+    ul = "itemizedlist",
+    ol = "orderedlist",
+    li = "listitem",
+    em = "emphasis",
+    i = "emphasis",
+    strong = "emphasis",
+    b = "emphasis",
+    sub = "subscript",
+    sup = "superscript",
+    pre = "literalLayout",
+    a = "ulink"
   )
 
   if (xml2::xml_type(node) == "text") {
@@ -80,7 +85,7 @@ convert <- function(node) {
     mapped <- tag_map[[tag]]
 
     children <- paste(
-      purrr::map_chr(xml2::xml_contents(node), convert),
+      purrr::map_chr(xml2::xml_contents(node), convert_xml_node),
       collapse = ""
     )
 
