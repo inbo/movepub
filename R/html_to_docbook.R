27598a18d21f4686d6a86ab1ee56539ef8e51125
Peter Desmet <peter.desmet@inbo.be>
2025-08-12 18:32:50 +0200

    Update documentation for html_to_docbook, rename param text to string

--- R/html_to_docbook.R
+++ R/html_to_docbook.R
@@ -1,51 +1,46 @@
-#' Convert HTML to DocBook XML
+#' Convert text with HTML to DocBook
 #'
-#' Converts HTML strings to DocBook XML strings.
-#' Handles the following tags: div, p, b, strong, i, em, a, ul, ol, li, sup,
-#' sub, pre and h1:h6.
-#' @param text String, may contain HTML.
-#' @return String, with HTML converted to DocBook XML.
+#' Converts HTML to [DocBook](https://docbook.org/) for a given text.
+#' Only a subset of HTML/DocBook tags are supported, see transformation details.
+#'
+#' @param string Text that may contain HTML.
+#' @return Text with HTML converted to DocBook.
 #' @family support functions
 #' @export
 #' @section Transformation details:
-#' `<title>`
-#' - `<h1>`
-#' `<para>`
-#' - `<div>`
-#' - `<p>`
-#' - `<h2>`to `<h6>`
+#' The function only converts HTML tags that can be translated to DocBook tags
+#' supported by EML for the [paragraph](
+#' https://eml.ecoinformatics.org/schema/eml-text_xsd.html#TextType_para)
+#' element.
+#' The following replacements are made:
 #'
-#' `<emphasis>`
-#' - `<b>`
-#' - `<strong>`
-#' - `<i>`
-#' - `<em>`
+#' HTML tag | DocBook tag
+#' --- | ---
+#' `<p>...</p>` | `<para>...</para>`
+#' `<div>...</div>` | `<para>...</para>`
+#' `<h2>...</h2>` | `<para>...</para>`
+#' `<h3>...</h3>` | `<para>...</para>`
+#' `<h4>...</h4>` | `<para>...</para>`
+#' `<h5>...</h4>` | `<para>...</para>`
+#' `<h6>...</h4>` | `<para>...</para>`
+#' `<ul>...</ul>` | `<itemizedlist>...</itemizedlist>`
+#' `<ol>...</ol>` | `<orderedlist>...</orderedlist>`
+#' `<li>...</li>` | `<listitem>...</listitem>`
+#' `<strong>...</strong>` | `<emphasis>...</emphasis>`
+#' `<b>...</b>` | `<emphasis>...</emphasis>`
+#' `<em>...</em>` | `<emphasis>...</emphasis>`
+#' `<i>...</i>` | `<emphasis>...</emphasis>`
+#' `<sub>...</sub>` | `<subscript>...</subscript>`
+#' `<sup>...</sup>` | `<superscript>...</superscript>`
+#' `<pre>...</pre>` | `<literalLayout>...</literalLayout>`
+#' `<a href="http://example.com">...</a>` | `<ulink url="https://example.com"><citetitle>...</citetitle></ulink>`
+#' `<h1>...</h1>` | `<title>...</title>`
 #'
-#' `<ulink>`
-#' - `<a href="...">`
-#'
-#' `<itemizedlist>`
-#' - `<ul>`
-#'
-#' `<orderedlist>`
-#' - `<ol>`
-#'
-#' `<listitem>`
-#' - `<li>`
-#'
-#' `<superscript>`
-#' - `<sup>`
-#'
-#' `<subscript>`
-#' - `<sub>`
-#'
-#' `<literalLayout>`
-#' - `<pre>`
 #' @examples
-#' html_to_docbook("<div>text - <b>bold</b></div>")
-html_to_docbook <- function(text) {
+#' html_to_docbook("<div>My <b>bold</b> text.</div>")
+html_to_docbook <- function(string) {
   # Necessary for empty values and non-HTML text
-  doc <- xml2::read_html(paste0("<root>", text, "</root>"))
+  doc <- xml2::read_html(paste0("<root>", string, "</root>"))
   root <- xml2::xml_find_first(doc, ".//body/root")
   paste(purrr::map_chr(xml2::xml_contents(root), convert), collapse = "")
 }
@@ -53,7 +48,6 @@ html_to_docbook <- function(text) {
 #' Helper function to recursively convert XML nodes to DocBook XML
 #'
 #' @param node An XML node to convert.
-#'
 #' @returns A string with HTML converted to Dokbook XML
 #' @noRd
 convert <- function(node) {
